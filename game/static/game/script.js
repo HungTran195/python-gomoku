4fbaddf71a1abe21a95e1279d8427a2a93250848
Tony Tran <quochung.064@gmail.com>
2021-04-23 13:10:38 -0500

    update js script file

--- game/static/game/script.js
+++ game/static/game/script.js
@@ -9,12 +9,17 @@ const my_score = document.getElementById('score1');
 const score_opponent = document.getElementById('score2');
 const chat_box = document.getElementById('chat-box');
 const chat_area = document.getElementById('chat-area');
+const result_banner = document.getElementById('result-banner');
+const replay_request = document.getElementById('replay-request');
+const replay_confirm = document.getElementById('replay-confirm');
+const wait_for_replay = document.getElementById('wait-for-replay');
 
-let move_index, isturn, player_name = '', is_host;
+let move_index, isturn, player_name = '';
+
 // Get game id from server
 const get_room_url = function () {
     let room_url = document.getElementById('room_url');
-    let game_id, turn;
+    let game_id, is_host;
     if (room_url) {
         room_url = room_url.textContent.split('/');
         if (room_url.length > 3) {
@@ -37,9 +42,9 @@ const get_room_url = function () {
     if (!game_id) {
         alert('There are no available room, please try again later');
     }
-    return game_id;
+    return [game_id, is_host];
 };
-const game_id = get_room_url();
+const [game_id, is_host] = get_room_url();
 
 // Initialise page with hidden tags
 const init = function (first_time_load) {
@@ -50,26 +55,25 @@ const init = function (first_time_load) {
     if (is_host) {
         starting_box.classList.add('hidden');
     }
+    replay_request.classList.remove('hidden');
+    result_banner.classList.add('hidden');
+    replay_confirm.classList.add('hidden');
+    wait_for_replay.classList.add('hidden');
     document.querySelector('.new-game-box').classList.add('hidden');
-    document.getElementById('replay-request').classList.remove('hidden');
-    document.getElementById('wait-for-replay').classList.add('hidden');
-    document.getElementById('replay-confirm').classList.add('hidden');
-    document.getElementById('result-banner').classList.add('hidden');
     document.getElementById('replay-container').classList.add('hidden');
 }
 init(true);
-const hide_tags_to_start_game = function (mode = '') {
+
+const hide_tags_and_start_game = function (mode = '') {
     if (mode == 'ai') {
         document.getElementById('choose-game-type').classList.add('hidden');
     }
     overlay.classList.add('hidden');
     starting_box.classList.add('hidden');
-    // document.getElementById('lobby').classList.add('hidden');
-
 }
 
 const play_with_ai = function () {
-    hide_tags_to_start_game('ai');
+    hide_tags_and_start_game('ai');
     isturn = 1;
     turn_sign.style.backgroundColor = '#77f077';
     socket.emit('start_game', { game_type: 'ai', game_id: game_id });
@@ -105,31 +109,7 @@ document.getElementById('get-name-box').addEventListener('keydown', (e) => {
     }
 })
 
-// Handle received message from server to start game
-socket.on('start_PvP_game', function (data) {
-    // Display error message if:
-    // Room is full or Wrong room id or Room is closed
-    if (data.err_msg) {
-        document.querySelector('.error').classList.remove('hidden');
-        // overlay.classList.remove('hidden');
-        $('#error_msg').append(data.err_msg + '<br>');
-    }
-    else {
 
-        isturn = data.is_turn;
-
-        if (isturn) turn_sign.style.backgroundColor = '#77f077';
-        else turn_sign.style.backgroundColor = '#9a9a9a';
-
-        document.getElementById('player1').children[0].textContent = player_name;
-        document.getElementById('player2').children[0].textContent = data.opponent_name;
-        document.querySelector('.bottom-bar').classList.remove('hidden');
-        document.querySelector('.chat-container').classList.remove('hidden');
-
-        hide_tags_to_start_game();
-    }
-
-});
 
 const send_msg = function () {
     let chat_msg = chat_box.value;
@@ -153,19 +133,16 @@ chat_box.addEventListener('keydown', (e) => {
     }
 });
 
-socket.on('new_msg', function (data) {
-    let chat_msg = data.chat_msg;
-    if (chat_msg) {
 
-        const markup = `
-            <div class="chat-msg-P1">
-                <p>${chat_msg}</p>
-            </div>
-            `;
-        chat_area.insertAdjacentHTML('beforeend', markup);
-        chat_area.scrollTop = chat_area.scrollHeight;
-    }
-});
+// Check every game cells to decide 'move' by turn
+for (const element of allCells) {
+    element.addEventListener('click', () => {
+        if (!element.textContent & isturn) {
+            let box_id = element.id;
+            socket.emit('move', { game_id: game_id, move_index: box_id });
+        }
+    });
+}
 
 
 // Change color of winning nodes
@@ -179,23 +156,24 @@ const draw_winner = function (line, is_winner) {
     if (line.length !== 0) {
         draw_winning_line(line, is_winner);
         if (is_winner) {
-            document.getElementById('result-banner').classList.remove('hidden');
-            document.getElementById('result-banner').children[0].textContent = 'You Win!'
+            result_banner.classList.remove('hidden');
+            result_banner.children[0].textContent = 'You Win!'
             let score = my_score.children[0].textContent;
             my_score.children[0].textContent = parseInt(score) + 1
         }
 
         else {
-            document.getElementById('result-banner').classList.remove('hidden');
-            document.getElementById('result-banner').children[0].textContent = 'You Lose!'
+            result_banner.classList.remove('hidden');
+            result_banner.children[0].textContent = 'You Lose!'
 
             let score = score_opponent.children[0].textContent;
             score_opponent.children[0].textContent = parseInt(score) + 1
         }
     }
     else {
-        document.getElementById('result-banner').classList.remove('hidden');
-        document.getElementById('result-banner').children[0].textContent = 'Game Draw!'
+        result_banner.classList.remove('hidden');
+        result_banner.children[0].textContent = 'Game Draw!'
+
     }
     window.setTimeout(() => {
         document.getElementById('replay-container').classList.remove('hidden')
@@ -211,14 +189,81 @@ const updateMove = function (turn_of_current_move, move_index) {
     }
 };
 
-// get message from server
+
+/* Function "request_replay": 
+    -- send request to sever
+    -- wait for response from server*/
+const request_replay = function () {
+    socket.emit('request_replay', { game_id: game_id });
+    replay_request.classList.add('hidden');
+    wait_for_replay.classList.remove('hidden');
+};
+
+/* Fucntion "accept_replay": 
+    -- send "accept" response to severz
+    -- wait for server to create a new match */
+const accept_replay = function () {
+    socket.emit('accept_replay', { game_id: game_id });
+    replay_confirm.classList.add('hidden');
+    wait_for_replay.classList.remove('hidden');
+};
+
+
+const display_error = function (err_msg) {
+    init(false);
+    overlay.classList.remove('hidden');
+    document.querySelector('.error').classList.remove('hidden');
+    $('#error_msg').append(err_msg + '<br>');
+}
+
+
+// Handle received message from server to start game
+socket.on('start_PvP_game', function (data) {
+    // Display error message if:
+    // Room is full or Wrong room id or Room is closed
+    if (data.err_msg) {
+        display_error(data.err_msg);
+    }
+    else {
+        isturn = data.is_turn;
+        if (isturn) turn_sign.style.backgroundColor = '#77f077';
+        else turn_sign.style.backgroundColor = '#9a9a9a';
+
+        document.getElementById('player1').children[0].textContent = player_name;
+        document.getElementById('player2').children[0].textContent = data.opponent_name;
+        document.querySelector('.bottom-bar').classList.remove('hidden');
+        document.querySelector('.chat-container').classList.remove('hidden');
+
+        hide_tags_and_start_game();
+    }
+
+});
+
+
+socket.on('new_msg', function (data) {
+    let chat_msg = data.chat_msg;
+    if (chat_msg) {
+
+        const markup = `
+            <div class="chat-msg-P1">
+                <p>${chat_msg}</p>
+            </div>
+            `;
+        chat_area.insertAdjacentHTML('beforeend', markup);
+        chat_area.scrollTop = chat_area.scrollHeight;
+    }
+});
+
+
+
+// get move from server
 socket.on('move', function (data) {
     // $('#log').append('<br>Received: ' + msg.data); 
-    if ('err_msg' in data) {
-        document.querySelector('.error').classList.remove('hidden');
-        $('#error_msg').append(data.err_msg + '<br>');
+    if (data.err_msg) {
+        display_error(data.err_msg);
     }
     updateMove(data.turn_of_current_move, data.move_index);
+    // Game end: display game draw or game win/lose
     if (data.is_turn == 2) {
         isturn = 0;
         draw_winner(data.winning_line_index, data.is_winner);
@@ -239,33 +284,15 @@ socket.on('end_game', function (data) {
     overlay.classList.toggle('hidden');
 })
 
-// The following code are used to handle "Play Again" request
-
-/* Function "request_replay": 
-    -- send request to sever
-    -- wait for response from server*/
-const request_replay = function () {
-    socket.emit('request_replay', { game_id: game_id });
-    document.getElementById('replay-request').classList.add('hidden');
-    document.getElementById('wait-for-replay').classList.remove('hidden');
-};
-
-/* Fucntion "accept_replay": 
-    -- send "accept" response to severz
-    -- wait for server to create a new match */
-const accept_replay = function () {
-    socket.emit('accept_replay', { game_id: game_id });
-    document.getElementById('replay-confirm').classList.add('hidden');
-    document.getElementById('wait-for-replay').classList.remove('hidden');
-};
 
 /* Socket listen on request_replay from server:
     -- render replay confirm box and send confirm msg to server */
 socket.on('request_replay', function (data) {
-    document.getElementById('replay-confirm').classList.remove('hidden');
-    document.getElementById('replay-request').classList.add('hidden');
+    replay_confirm.classList.remove('hidden');
+    replay_request.classList.add('hidden');
 
 });
+
 
 /* Socket listen on a msg "replay" from server:
     -- reset play board and start a new game  */
@@ -277,23 +304,11 @@ socket.on('replay', function (data) {
         element.textContent = '';
     }
     init(false);
-
-
     isturn = data.is_turn;
-
     if (isturn) turn_sign.style.backgroundColor = '#77f077';
     else turn_sign.style.backgroundColor = '#9a9a9a';
 
     document.getElementById('replay-container').classList.add('hidden');
 });
 
-// Check every game cells to decide 'move' by turn
-for (const element of allCells) {
-    element.addEventListener('click', () => {
-        if (!element.textContent & isturn) {
-            let box_id = element.id;
-            socket.emit('move', { game_id: game_id, move_index: box_id });
-        }
-    });
-}
 
